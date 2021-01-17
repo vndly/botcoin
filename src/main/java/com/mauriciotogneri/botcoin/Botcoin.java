346b0570cc969c8b80f2e13baa565c99b97f2b87
Mauricio Togneri <mauricio.togneri@gmail.com>
2021-01-17 11:45:43 +0100

    Code cleanup

--- src/main/java/com/mauriciotogneri/botcoin/Botcoin.java
+++ src/main/java/com/mauriciotogneri/botcoin/Botcoin.java
@@ -9,21 +9,46 @@ import com.mauriciotogneri.botcoin.strategies.sell.SellStrategy;
 
 public class Botcoin
 {
-    public static void main(String[] args) throws Exception
-    {
-        PriceProvider provider = new FileProvider("input/prices.csv");
-        BuyStrategy buyStrategy = new BasicBuyStrategy();
-        SellStrategy sellStrategy = new BasicSellStrategy();
+    private final PriceProvider priceProvider;
+    private final BuyStrategy buyStrategy;
+    private final SellStrategy sellStrategy;
+    private final Wallet wallet;
 
+    public Botcoin(PriceProvider priceProvider, BuyStrategy buyStrategy, SellStrategy sellStrategy, Wallet wallet)
+    {
+        this.priceProvider = priceProvider;
+        this.buyStrategy = buyStrategy;
+        this.sellStrategy = sellStrategy;
+        this.wallet = wallet;
+    }
+
+    public void start() throws Exception
+    {
         while (true)
         {
-            float price = provider.price();
+            float price = priceProvider.price();
 
             float buyAmount = buyStrategy.buy();
             float sellAmount = sellStrategy.sell();
 
-
+            System.out.println(price);
         }
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        PriceProvider priceProvider = new FileProvider("input/prices.csv");
+        BuyStrategy buyStrategy = new BasicBuyStrategy();
+        SellStrategy sellStrategy = new BasicSellStrategy();
+        Wallet wallet = new Wallet(0, 0);
+
+        Botcoin botcoin = new Botcoin(
+                priceProvider,
+                buyStrategy,
+                sellStrategy,
+                wallet
+        );
+        botcoin.start();
     }
 
     /*private void collectPrices() throws Exception
---
+++ src/main/java/com/mauriciotogneri/botcoin/Wallet.java
@@ -0,0 +1,13 @@
+package com.mauriciotogneri.botcoin;
+
+public class Wallet
+{
+    private float amount1;
+    private float amount2;
+
+    public Wallet(float amount1, float amount2)
+    {
+        this.amount1 = amount1;
+        this.amount2 = amount2;
+    }
+}
