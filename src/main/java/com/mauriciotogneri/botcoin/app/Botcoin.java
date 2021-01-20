16f3dcc079988f0bd48542218166631a5d50b6f2
Mauricio Togneri <mauricio.togneri@gmail.com>
2021-01-20 23:31:46 +0100

    Code cleanup

--- src/main/java/com/mauriciotogneri/botcoin/app/Botcoin.java
+++ src/main/java/com/mauriciotogneri/botcoin/app/Botcoin.java
@@ -56,41 +56,58 @@ public class Botcoin
         Log log = new Log("output/logs.txt");
 
         double maxProfit = 0;
-        int bestI = 0;
-        int bestJ = 0;
+        int bestA = 0;
+        int bestB = 0;
         float bestX = 0;
+        float bestY = 0;
+        int index = 1;
 
-        PriceProvider priceProvider = new FileProvider(firstPriceProvider.prices());
-
-        float minPercentageDown = 0.01f;
-        float percentageBuyMultiplier = 70;
-        float minEurToSpend = 10;
-
-        float minPercentageUp = 0.05f;
-        float percentageSellMultiplier = 100;
-        float sellAllLimit = 0.001f;
-        float minEurToGain = 10;
-
-        BasicWallet wallet = new BasicWallet(5000, 0, log);
-        BuyStrategy buyStrategy = new BasicBuyStrategy(wallet, minPercentageDown, percentageBuyMultiplier, minEurToSpend);
-        SellStrategy sellStrategy = new BasicSellStrategy(wallet, minPercentageUp, percentageSellMultiplier, sellAllLimit, minEurToGain);
-
-        Botcoin botcoin = new Botcoin(
-                wallet,
-                priceProvider,
-                buyStrategy,
-                sellStrategy
-        );
-        float totalBalance = botcoin.start();
-
-        if (totalBalance > maxProfit)
+        for (int a = 1; a <= 100; a += 10)
         {
-            maxProfit = totalBalance;
-            //bestI = i;
-            //bestJ = j;
-            // bestX = x;
+            for (int b = 1; b <= 100; b += 10)
+            {
+                for (int x = 1; x <= 100; x += 10)
+                {
+                    for (int y = 1; y <= 100; y += 10)
+                    {
+                        System.out.println((index++ / 100) + "%");
+
+                        PriceProvider priceProvider = new FileProvider(firstPriceProvider.prices());
+
+                        float minPercentageDown = (float) x / 100f; //0.01f;
+                        float percentageBuyMultiplier = a; //70;
+                        float minEurToSpend = 10;
+
+                        float minPercentageUp = (float) y / 100; //0.05f;
+                        float percentageSellMultiplier = b; //100;
+                        float sellAllLimit = 0.001f;
+                        float minEurToGain = 10;
+
+                        BasicWallet wallet = new BasicWallet(5000, 0, log);
+                        BuyStrategy buyStrategy = new BasicBuyStrategy(wallet, minPercentageDown, percentageBuyMultiplier, minEurToSpend);
+                        SellStrategy sellStrategy = new BasicSellStrategy(wallet, minPercentageUp, percentageSellMultiplier, sellAllLimit, minEurToGain);
+
+                        Botcoin botcoin = new Botcoin(
+                                wallet,
+                                priceProvider,
+                                buyStrategy,
+                                sellStrategy
+                        );
+                        float totalBalance = botcoin.start();
+
+                        if (totalBalance > maxProfit)
+                        {
+                            maxProfit = totalBalance;
+                            bestA = a;
+                            bestB = b;
+                            bestX = x;
+                            bestY = y;
+                        }
+                    }
+                }
+            }
         }
 
-        System.out.println(String.format("MAX PROFIT %s %s %s %s", maxProfit, bestI, bestJ, bestX));
+        System.out.println(String.format("MAX PROFIT %s %s %s %s %s", maxProfit, bestA, bestB, bestX, bestY));
     }
 }
