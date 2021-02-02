c13c481edaa197de039235854b4a1cde171b0c7c
Mauricio Togneri <mauricio.togneri@gmail.com>
2021-02-02 23:15:21 +0100

    Improved chart

--- html/chart.js
+++ html/chart.js
@@ -5,28 +5,7 @@ function show(pricesFile)
     loadPrices(pricesFile, function(response)
     {
         var data = JSON.parse('[' + response + ']')
-
-        /*var select = document.getElementById('select')
-        select.addEventListener('change', function()
-        {
-            processResult(data[select.value])
-        })
-
-        for (var index in data)
-        {
-            var entry = data[index]
-            var option = document.createElement('option')
-            option.value = index
-            option.text = entryName(index, entry)
-            select.add(option)
-        }
-
-        if (data.length)
-        {
-            select.value = 0
-            processResult(data[select.value])
-        }*/
-        //console.log(response)
+		processResult(data)
     })
 }
 
@@ -45,157 +24,53 @@ function loadPrices(file, callback)
     xobj.send(null)
 }
 
-/*function entryName(index, entry)
-{
-    return (parseInt(index) + 1) + '. ' + operationsSummary(entry.operations) + ' | ' + entry.totalProfit + ' | ' + entry.averagePercentageProfit.toFixed(2) + '%'
-}*/
-
-/*function processResult(json)
-{
-    var fileName = '../../data/' + json.input.pair + '_' + json.input.interval + '.json'
-
-	fillInputTable(json)
-
-    loadJSON(fileName, function(response)
-    {
-        var data = JSON.parse(response)
-
-        var history = priceHistory(data)
-        var events = eventMarkers(json)
-        
-        var markersWB = markersByType(TYPE_WB, events)
-        var markersTB = markersByType(TYPE_TB, events)
-        var markersB  = markersByType(TYPE_B,  events)
-        var markersWS = markersByType(TYPE_WS, events)
-        var markersTS = markersByType(TYPE_TS, events)
-        var markersS  = markersByType(TYPE_S,  events)
-
-        render(history, json.sma1, json.sma2, json.ema1, json.ema2, markersWB, markersTB, markersB, markersWS, markersTS, markersS)
-    })
-}
-
-function fillInputTable(result)
-{
-	fillInputCell('input.operations', operationsSummary(result.operations))
-	fillInputCell('input.totalProfit', result.totalProfit)
-	fillInputCell('input.averagePercentage', result.averagePercentageProfit.toFixed(2) + '%')
-	fillInputCell('input.source', result.input.source)
-	fillInputCell('input.pair', result.input.pair)
-	fillInputCell('input.interval', result.input.interval)
-	fillInputCell('input.maxCost', result.input.maxCost)
-	fillInputCell('input.buyStrategy', result.input.buyStrategy)
-	fillInputCell('input.buyValue', round(result.input.buyValue))
-	fillInputCell('input.trailingBuy', round(result.input.trailingBuy))
-	fillInputCell('input.sellStrategy', result.input.sellStrategy)
-	fillInputCell('input.sellValue', round(result.input.sellValue))
-	fillInputCell('input.trailingProfit', round(result.input.trailingProfit))
-	fillInputCell('input.stopLossTrigger', round(result.input.stopLossTrigger))
-	fillInputCell('input.smaPeriod', result.input.smaPeriod)
-	fillInputCell('input.sma1', result.input.sma1)
-	fillInputCell('input.sma2', result.input.sma2)
-	fillInputCell('input.smaCrossCandles', result.input.smaCrossCandles)
-	fillInputCell('input.emaPeriod', result.input.emaPeriod)
-	fillInputCell('input.ema1', result.input.ema1)
-	fillInputCell('input.ema2', result.input.ema2)
-	fillInputCell('input.emaCrossCandles', result.input.emaCrossCandles)
-	fillInputCell('input.bbPeriod', result.input.bbPeriod)
-	fillInputCell('input.bbSma', result.input.bbSma)
-}
-
-function operationsSummary(operations)
-{
-	var positive = 0
-	var negative = 0
-	
-	for (var index in operations)
-	{
-		var events = operations[index].events
-		var last = events[events.length-1]
-		
-		if (last.totalProfit > 0)
-		{
-			positive++
-		}
-		else
-		{
-			negative++
-		}
-	}
-	
-	return operations.length + ' (+' + positive + '/-' + negative + '/' + parseInt(positive/operations.length*100) + '%)'
-}
-
-function round(value)
-{
-	return Math.round(value * 100) / 100
-}
-
-function fillInputCell(name, value)
-{
-	document.getElementById(name).innerHTML = value
-}
-
 function priceHistory(json)
 {
-    var list = []
+    const list = []
 
     for (var index in json)
     {
         var element = json[index]
-        list.push([element.time, element.price])
+        list.push([element.timestamp, element.price])
     }
 
     return list
 }
 
-function eventMarkers(json)
+function buysHistory(json)
 {
-    var markers = []
+    const list = []
 
-    var markerIndex = 1
-
-    for (var index in json.operations)
+    for (var index in json)
     {
-        var operation = json.operations[index]
+        var element = json[index]
 
-        for (var i in operation.events)
+        if (element.buy)
         {
-            var event = operation.events[i]
-            var type = eventType(event)
-        
-            markers.push(
-            {
-                type: type,
-                x: event.time,
-                title: markerIndex + type,
-                text: eventText(event)
-            })
+	        list.push({
+	            type: 'buy',
+	            x: element.timestamp,
+	            y: element.price,
+	            title: 'buy: ' + index,
+	            text: 'yes'
+	        })
         }
-
-        markerIndex++
     }
 
-    return markers
+    return list
 }
 
-function markersByType(type, list)
+function processResult(json)
 {
-	var markers = []
-	
-	for (var index in list)
-	{
-		var element = list[index]
-		
-		if (element.type == type)
-		{
-			markers.push(element)
-		}
-	}
-	
-	return markers
+    const prices = priceHistory(json)
+    console.log(prices)
+    const buys = buysHistory(json)
+    console.log(buys)
+
+    render(prices, buys)
 }
 
-function render(data, sma1, sma2, ema1, ema2, markersWB, markersTB, markersB, markersWS, markersTS, markersS)
+function render(prices, buys)
 {
     chart = Highcharts.stockChart('container', {
 
@@ -222,128 +97,17 @@ function render(data, sma1, sma2, ema1, ema2, markersWB, markersTB, markersB, ma
         series: [
             {
                 name: 'Price',
-                data: data,
+                data: prices,
                 id: 'dataseries'
             },
             {
-                name: 'SMA 1',
-                data: sma1,
-                id: 'sma1',
-                type: 'spline',
-                color: '#da6767',
-                visible: isChecked('sma')
-            },
-            {
-                name: 'SMA 2',
-                data: sma2,
-                id: 'sma2',
-                type: 'spline',
-                dashStyle: 'Dot',
-                color: '#da6767',
-				visible: isChecked('sma')
-            },
-            {
-                name: 'EMA 1',
-                data: ema1,
-                id: 'ema1',
-                type: 'spline',
-                color: '#61a053',
-                visible: isChecked('ema')
-            },
-            {
-                name: 'EMA 2',
-                data: ema2,
-                id: 'ema2',
-                type: 'spline',
-                dashStyle: 'Dot',
-                color: '#61a053',
-                visible: isChecked('ema')
-            },
-            {
                 type: 'flags',
-                data: markersWB,
+                data: buys,
                 onSeries: 'dataseries',
                 shape: 'squarepin',
                 color: '#00DB04',
-                width: 30,
-                visible: isChecked('eventWB')
-            },
-	        {
-	            type: 'flags',
-	            data: markersTB,
-	            onSeries: 'dataseries',
-	            shape: 'squarepin',
-	            color: '#00A603',
-	            width: 30,
-	            visible: isChecked('eventTB')
-	        },
-	        {
-	            type: 'flags',
-	            data: markersB,
-	            onSeries: 'dataseries',
-	            shape: 'squarepin',
-	            color: '#006B02',
-	            width: 30,
-	            visible: isChecked('eventB')
-	        },
-	        {
-	            type: 'flags',
-	            data: markersWS,
-	            onSeries: 'dataseries',
-	            shape: 'squarepin',
-	            color: '#F50000',
-	            width: 30,
-	            visible: isChecked('eventWS')
-	        },
-	        {
-	            type: 'flags',
-	            data: markersTS,
-	            onSeries: 'dataseries',
-	            shape: 'squarepin',
-	            color: '#BF0000',
-	            width: 30,
-	            visible: isChecked('eventTS')
-	        },
-	        {
-	            type: 'flags',
-	            data: markersS,
-	            onSeries: 'dataseries',
-	            shape: 'squarepin',
-	            color: '#9C0000',
-	            width: 30,
-	            visible: isChecked('eventS')
-	        }
+                width: 30
+            }
         ]
     })
 }
-
-function showSeries(indexA, indexB, checkbox)
-{
-	if (checkbox.checked)
-    {
-        chart.series[indexA].show()
-        chart.series[indexB].show()
-    }
-    else
-    {
-        chart.series[indexA].hide()
-        chart.series[indexB].hide()
-    }
-}
-
-function isChecked(id)
-{
-	return document.getElementById(id).checked
-}
-
-function showEvent(index, checkbox)
-{
-	if (checkbox.checked)
-    {
-        chart.series[index].show()
-    }
-    else
-    {
-        chart.series[index].hide()
-    }
-}*/
