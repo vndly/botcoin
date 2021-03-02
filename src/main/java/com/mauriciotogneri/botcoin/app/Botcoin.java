bb7d72d555ae6d233432f6812d49e532dd9d9333
Mauricio Togneri <mauricio.togneri@gmail.com>
2021-03-02 21:47:12 +0100

    Code cleanup

--- src/main/java/com/mauriciotogneri/botcoin/app/Botcoin.java
+++ src/main/java/com/mauriciotogneri/botcoin/app/Botcoin.java
@@ -30,7 +30,6 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-// TODO: keep track of latest balance B to not to buy more than we can
 // TODO: implement trailing when selling
 public class Botcoin
 {
